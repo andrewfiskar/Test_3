6c39bdb0318e4b38843c5ef7fd0993796b9ec067
andrewfiskar <andrew.robertson@unitetheunion.org>
2016-01-21 11:41:56 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -15,7 +15,6 @@ root = lxml.html.fromstring(html)
 kik = root.cssselect('div')
 #change 'td' to a different selector to scrape something else on the page
 #lxml.html  - Parsing HTML (breaking it up and getting to a bit of it. lxml.html.fromstring 
-# – turns something from a string into something else.
 #tds = root.cssselect td – cssselect is attached to an lxml object
 #cssselect is attached to an lxml object, so look in lxml documentation
 #use .cssselect method on root to grab 'td' tags and put in tds
